9007fcffec64840d8cf5a1f40710491f34fa2aff
NTDKhoa04 <nguyenthaidangkhoa04@gmail.com>
2025-10-19 13:56:53 +0700

    feat: Added email verification logic

--- src/mailing-service/mailing-service.controller.ts
+++ src/mailing-service/mailing-service.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+import { SuccessResponse } from 'src/shared/classes/success-response.class';
 import { MailingServiceService } from './mailing-service.service';
 
 @Controller({
@@ -6,11 +9,29 @@ import { MailingServiceService } from './mailing-service.service';
   path: 'mail',
 })
 export class MailingServiceController {
-  constructor(private readonly mailingService: MailingServiceService) {}
+  constructor(
+    private readonly mailingService: MailingServiceService,
+    private readonly configService: ConfigService,
+  ) {}
 
-  @Post()
-  async sendTestMail() {
-    const result = await this.mailingService.sendTestEmail();
-    return result;
+  @Get('/verify-email')
+  async verifyEmail(@Query('token') token: string, @Res() res: Response) {
+    var redirectUrl = this.configService.getOrThrow('BASE_FRONTEND_URL');
+
+    try {
+      await this.mailingService.verifyEmail(token);
+      redirectUrl += '/mail/verification-success';
+      res.redirect(redirectUrl);
+    } catch (err) {
+      var problem = err.response.errors[0].message;
+      redirectUrl += `/mail/verification-failed?problem=${problem}`;
+      res.redirect(redirectUrl);
+    }
+  }
+
+  @Post('/resend-verification')
+  async resendVerification(@Body('email') email: string) {
+    await this.mailingService.resendVerificationEmail(email);
+    return new SuccessResponse('Verification email resent', null);
   }
 }
--- src/mailing-service/mailing-service.service.ts
+++ src/mailing-service/mailing-service.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Options, SentMessageInfo } from 'nodemailer/lib/smtp-transport';
 import { MailTemplatesEnum } from 'src/shared/enum/mail-templates.enum';
@@ -6,11 +12,19 @@ import * as nodemailer from 'nodemailer';
 import * as fs from 'fs';
 import * as path from 'path';
 import {
-  AdminWarningReplacementsDto,
   EmailVerificationReplacementsDto,
   MailReplacementsDto,
 } from './dto/mail-replacements.dto';
 import MailContentDto from './dto/mail-content.dto';
+import * as Redis from 'redis';
+import * as crypto from 'crypto';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from 'src/user/model/user.model';
+import {
+  ValidationError,
+  ValidationErrorDetail,
+} from 'src/shared/classes/validation-error.class';
+import { VerificationProblemsEnum } from 'src/shared/enum/verification-problems.enum';
 
 @Injectable()
 export class MailingServiceService {
@@ -19,7 +33,17 @@ export class MailingServiceService {
     Options
   >;
 
-  constructor(private readonly configService: ConfigService) {
+  private readonly redisClient: Redis.RedisClientType;
+  private TOKEN_TTL_SECONDS: number = 60;
+  private readonly TOKEN_PREFIX: string = 'email_verify:token:';
+  private readonly LOOKUP_TOKEN_PREFIX: string = 'email_verify:lookup:';
+
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+
+    private readonly configService: ConfigService,
+  ) {
     this.transporter = nodemailer.createTransport({
       host: configService.getOrThrow('MAILTRAP_HOST'),
       port: configService.getOrThrow('MAILTRAP_PORT'),
@@ -28,6 +52,12 @@ export class MailingServiceService {
         pass: configService.getOrThrow('MAILTRAP_PASS'),
       },
     });
+
+    this.redisClient = Redis.createClient({ url: 'redis://redis:6379' });
+    this.redisClient.on('error', (err) =>
+      console.error('Redis Client Error', err),
+    );
+    this.redisClient.connect();
   }
 
   private addReplacements(
@@ -55,27 +85,157 @@ export class MailingServiceService {
     return htmlStringWithReplacements;
   }
 
-  public async sendTestEmail() {
+  private async generateToken(
+    userId: string,
+    email: string,
+  ): Promise<string | undefined> {
     try {
-      const replacements: AdminWarningReplacementsDto = {
-        username: 'Khoa',
-        postLink: 'https://bloggin.blog/posts/123',
+      var token = crypto.randomBytes(32).toString('hex');
+
+      //Official token
+      await this.redisClient.set(this.TOKEN_PREFIX + token, userId, {
+        EX: this.TOKEN_TTL_SECONDS,
+      });
+
+      //Lookup token
+      await this.redisClient.set(this.LOOKUP_TOKEN_PREFIX + email, email, {
+        EX: this.TOKEN_TTL_SECONDS,
+      });
+
+      return token;
+    } catch (err) {
+      console.error('Error while generating token:', err);
+    }
+  }
+
+  public async sendVerificationEmail(
+    userId: string,
+    displayName: string,
+    email: string,
+  ) {
+    try {
+      var verifyToken = await this.generateToken(userId, email);
+
+      var baseBackendUrl = this.configService.getOrThrow('BASE_BACKEND_URL');
+
+      var replacements: EmailVerificationReplacementsDto = {
+        username: displayName,
+        verifyLink: `${baseBackendUrl}/mail/verify-email?token=${verifyToken}`,
       };
-      const template = this.getHtmlString(
-        MailTemplatesEnum.ADMIN_WARNING,
+
+      const htmlString = this.getHtmlString(
+        MailTemplatesEnum.MAIL_VERIFICATION,
         replacements,
       );
 
       const mailContent: MailContentDto = {
         from: 'info@bloggin.blog',
-        to: 'nguyenthaidangkhoa04@gmail.com',
+        to: email,
         subject: 'Test Email with templates',
-        html: template,
+        html: htmlString,
       };
 
-      const results = await this.transporter.sendMail(mailContent);
+      // const results = await this.transporter.sendMail(mailContent);
     } catch (error) {
       console.error('Error sending email:', error);
     }
+  }
+
+  public async resendVerificationEmail(email: string) {
+    var lookupToken = await this.redisClient.get(
+      this.LOOKUP_TOKEN_PREFIX + email,
+    );
+
+    if (lookupToken != null) {
+      throw new ConflictException(
+        new ValidationError(
+          [
+            new ValidationErrorDetail(
+              'email',
+              'Verification email is still valid. Check your email or wait a moment before trying again.',
+            ),
+          ],
+          HttpStatus.CONFLICT,
+        ),
+      );
+    }
+
+    var user = await this.userModel.findOne({ where: { email: email } });
+
+    if (!user) {
+      throw new NotFoundException(
+        new ValidationError(
+          [new ValidationErrorDetail('email', 'User not found')],
+          HttpStatus.NOT_FOUND,
+        ),
+      );
+    }
+
+    if (user.isVerified) {
+      throw new BadRequestException(
+        new ValidationError(
+          [new ValidationErrorDetail('email', 'User already been verified')],
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    }
+
+    await this.sendVerificationEmail(user.id, user.username, user.email);
+  }
+
+  public async verifyEmail(token: string): Promise<void> {
+    var userId = await this.redisClient.get(this.TOKEN_PREFIX + token);
+
+    if (!userId) {
+      throw new BadRequestException(
+        new ValidationError(
+          [
+            new ValidationErrorDetail(
+              'token',
+              VerificationProblemsEnum.TOKEN_INVALID,
+            ),
+          ],
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    }
+
+    var user = await this.userModel.findByPk(userId);
+
+    if (!user) {
+      throw new NotFoundException(
+        new ValidationError(
+          [
+            new ValidationErrorDetail(
+              'email',
+              VerificationProblemsEnum.USER_NOT_FOUND,
+            ),
+          ],
+          HttpStatus.NOT_FOUND,
+        ),
+      );
+    }
+
+    if (user.isVerified) {
+      throw new BadRequestException(
+        new ValidationError(
+          [
+            new ValidationErrorDetail(
+              'email',
+              VerificationProblemsEnum.USER_ALREADY_VERIFIED,
+            ),
+          ],
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
+    }
+
+    // await this.userModel.update(
+    //   { isVerified: true },
+    //   { where: { id: userId } },
+    // );
+
+    await this.redisClient.del(this.TOKEN_PREFIX + token);
+    await this.redisClient.del(this.LOOKUP_TOKEN_PREFIX + user.email);
   }
 }
